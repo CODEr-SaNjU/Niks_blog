48ec59026753eeb9391a4fa0f51c1b756b030fcb
coder-sanju <sanju2help@gmail.com>
2020-08-14 23:54:24 +0530

    update

--- nik_blogs/nik_blogs/settings.py
+++ nik_blogs/nik_blogs/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3x*ufv1r*kfk*hemp+pjfj%znv9b-ec$i!n!c^y6=*sdhn$*-q'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', '3x*ufv1r*kfk*hemp+pjfj%znv9b-ec$i!n!c^y6=*sdhn$*-q')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
